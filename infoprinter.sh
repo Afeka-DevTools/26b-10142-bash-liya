e11c0650bc5285553f083554aedbc9abf02e493f
liya-joni <Liya.joni@s.Afeka.ac.il>
2026-06-25 12:37:49 +0300

    Create infoprinter.sh

---
+++ infoprinter.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "=== Current User Information ==="
+echo "Username:      $(whoami)"
+echo "Home Directory: $HOME"
+echo "User Groups:    $(groups)"
+echo "Current Shell:  $SHELL"
+echo "================================"
